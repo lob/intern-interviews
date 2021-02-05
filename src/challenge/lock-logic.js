7de0b1cb5497f302f5d2f24dc9e344912b5093c4
qiuyutianwen <50857304+qiuyutianwen@users.noreply.github.com>
2021-02-05 04:07:43 -0800

    Yu Qiu intern challenge

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -1,13 +1,13 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
   wheels: [0, 0, 0, 0]
-})
+});
 
-function changeDialValue (index, incrementBy) {
+function changeDialValue(index, incrementBy) {
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
   // it will be called with a wheel index and an amount to change the value by
@@ -25,8 +25,35 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+
+  //apply change to lockState
+  lockState.wheels[index] += incrementBy;
+
+  //wheels value should be in the range of [0, 9]
+  if (lockState.wheels[index] === -1) {
+    lockState.wheels[index] = 9;
+  } else if (lockState.wheels[index] === 10) {
+    lockState.wheels[index] = 0;
+  }
+
+  //unlock the combo lock and enable redirect
+  if (checkSecret(SECRET_COMBO, lockState.wheels)) {
+    lockState.locked = false;
+    redirect("yu-qiu");
+  }
+}
+
+function checkSecret(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
 
 // let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
