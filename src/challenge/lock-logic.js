22fe9c8d7c8f1156d9b0646eb21f5c255323bf08
Ruotian Gao <rug024@ucsd.edu>
2021-02-04 17:08:32 -0800

    Update lock-logic.js

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -25,7 +25,7 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
-  
+
   lockState.wheels[index] = lockState.wheels[index]+incrementBy
 
   if (lockState.wheels[index]==-1) {
