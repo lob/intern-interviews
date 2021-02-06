aad2c105f5eba098ab2a4de785cb9351b3d15c60
osha7 <ogroetz@gmail.com>
2021-02-06 09:54:58 -0700

    all functions working

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,38 +8,28 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // body > div.combination > div:nth-child(4) > input
-  // document.getElementsByClassName("digit")[index]
-  // debugger
 
   dialValue = parseInt(document.getElementsByClassName("digit")[index].value)
 
-  if (incrementBy === 1 && parseInt(document.getElementsByClassName("digit")[index].value) < 9) {
-    document.getElementsByClassName("digit")[index].value = parseInt(document.getElementsByClassName("digit")[index].value) + incrementBy
+  if (incrementBy === 1 && dialValue < 9) {
+    newIndexValue = document.getElementsByClassName("digit")[index].value = dialValue + incrementBy
+    lockState.wheels[index] = newIndexValue
   }
   
   if (incrementBy === -1 && dialValue > 0) {
-    document.getElementsByClassName("digit")[index].value = parseInt(document.getElementsByClassName("digit")[index].value) + incrementBy
+    document.getElementsByClassName("digit")[index].value = dialValue + incrementBy
+    lockState.wheels[index] = newIndexValue
   }
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+ console.log(lockState.wheels)
+ console.log(lockState.wheels === SECRET_COMBO)
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+ if (lockState.wheels[0] === SECRET_COMBO[0] && lockState.wheels[1] === SECRET_COMBO[1] && lockState.wheels[3] === SECRET_COMBO[3] && lockState.wheels[3] === SECRET_COMBO[3]) {
+  lockState.locked = false
+  redirect('osha-groetz')
+ }
+
 }
 
-// let our other modules find our functions
 window.lockState = lockState
 window.changeDialValue = changeDialValue
