3171061de57dd77e96d00819f89b6af9db88070f
Kathy Morawski <kathymorawski@gmail.com>
2021-02-07 15:42:42 -0600

    Feat: Add functionality to unlock for user to unlock our combolock and redirect them to the intern page

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -7,24 +7,22 @@ const lockState = window.mobx.observable({
   wheels: [0, 0, 0, 0]
 })
 
-function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+function changeDialValue(index, incrementBy) {
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+  //Find value at the selected Index, then add incremeneted value to it
+  let selectedIndexValue = lockState.wheels[index];
+  let newValue = selectedIndexValue += incrementBy;
+  // Remove old value at the selected index, and replace it with our new value
+  lockState.wheels.splice(index, 1, newValue);
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+  //Compare our current wheels array values to our secret combo, returning a new array with 'true' in that index if the values match
+  let compareToSecretCombo = lockState.wheels.map((value, index) => { if (value === SECRET_COMBO[index]) { return true } })
+
+  //Once every value in our new array from our .map method, set locked state to false and redirect to our intern page
+  if (compareToSecretCombo.every((value) => (value === true))) {
+    lockState.locked = false;
+    redirect('larry-lobster')
+  }
 }
 
 // let our other modules find our functions
