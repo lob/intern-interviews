e7dac074c6f2d9c093354af769c42d5db96fe665
irosen419 <irosen419@gmail.com>
2021-02-08 15:56:16 -0500

    eslinted. vscode would not let me put a space before function-parens

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,15 +8,12 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue(index, incrementBy) {
-
   // increment (or decrement) the given index by the passed in value of incrementBy
   lockState.wheels[index] += incrementBy
-
   // if lockState.wheels === SECRET_COMBO, unlock the lock
   if (lockState.wheels[0] === SECRET_COMBO[0] && lockState.wheels[1] === SECRET_COMBO[1] && lockState.wheels[2] === SECRET_COMBO[2] && lockState.wheels[3] === SECRET_COMBO[3]) {
     lockState.locked = false
   }
-
   // if the lock if unlocked, redirect to profile
   if (!lockState.locked) {
     redirect('Ian Rosen')
