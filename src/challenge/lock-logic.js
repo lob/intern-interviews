dde3326346498fdeb99125db5bbcbedcd3814672
Cooper Vos <coopervos1@gmail.com>
2021-02-08 14:46:26 -0600

    application

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,23 +8,34 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+  // clicking the up arrow
+  if(incrementBy == 1){
+    // if 9 go to 0
+    if(lockState.wheels[index] == 9){
+      lockState.wheels[index] = 0
+    }
+    // otherwise increment by 1
+    else{
+      lockState.wheels[index] += 1
+    }
+  }
+  // clicking the down arrow
+  else{
+    // if 0 go to 9
+    if(lockState.wheels[index] == 0){
+      lockState.wheels[index] = 9
+    }
+    // otherwise decrement by 1
+    else{
+      lockState.wheels[index] -= 1
+    }
+  }
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
-
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+  // CHECKING IF CODE MATCH
+  if(lockState.wheels[0] == SECRET_COMBO[0] & lockState.wheels[1] == SECRET_COMBO[1] & lockState.wheels[2] == SECRET_COMBO[2] & lockState.wheels[3] == SECRET_COMBO[3]){
+    lockState.locked = false
+    redirect('cooper-vos')
+  }
 }
 
 // let our other modules find our functions
