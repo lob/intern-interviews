55e67c0b10870117863d319a4fca10c6b61915eb
Emma Hogan <em30hoga@siena.edu>
2021-02-09 16:28:33 -0500

    Add documentation

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,23 +8,13 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue(index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
-
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+  // Adjust the value of wheels at the given index by the amount specified by incrementBy
   lockState.wheels[index] += incrementBy;
 
+  var match = true; // Whether the wheels match the secret combo
+  var i = 0;  // Index to check
 
-  var match = true;
-  var i = 0;
+  // Keep checking the next index of the wheel for equality with the secret combo until a wheel doesn't match
   while (i < SECRET_COMBO.length && match) {
     if (lockState.wheels[i] != SECRET_COMBO[i]) {
       match = false;
@@ -32,15 +22,11 @@ function changeDialValue(index, incrementBy) {
     i++;
   }
 
+  // If match has not been set to false, then all wheels match secret combo
   if (match) {
-    lockState.locked = false;
-    redirect('emma-hogan');
+    lockState.locked = false; // Unlock combo lock and 
+    redirect('emma-hogan'); // Redirect to about me page
   }
-
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
 }
 
 // let our other modules find our functions
