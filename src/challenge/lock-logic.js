62350d8ecaba5fe61c19451a573bb9735b0713dc
Tickloop <pulkitarya2000@gmail.com>
2021-02-11 03:12:20 +0530

    Solved the puzzle

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,23 +8,42 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+  //we assume that the index belongs to this range of values: [0, 1, 2, 3]
+  const updatedValue = lockState.wheels[index] + incrementBy;
+  
+  //we will create a roll over/under if the wheel is at 0 or 9
+  if(updatedValue > 9){
+    //since we were already at 9, we should roll over to a 0 as we are trying to implement a wheel
+    lockState.wheels[index] = 0;
+  }else if(updatedValue < 0){
+    //since we were already at 0, we should roll under to a 9 as we are trying to implement a wheel
+    lockState.wheels[index] = 9;
+  }else{
+    //this is the general case where we can assign the updated value directly to the wheels
+    lockState.wheels[index] = updatedValue;
+  }
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+  //now we check whether our wheels array is permisible
+  //we should only check all values if the value at the current updated index matches the value in the SECRET_COMBO
+  if(lockState.wheels[index] === SECRET_COMBO[index]){
+    //we loop through all values
+    //we assume that the lock should be unlocked
+    let isSame = true;
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+    for(let i = 0;i < lockState.wheels.length;i++){
+      if(lockState.wheels[i] !== SECRET_COMBO[i]){
+        //since this is the only time we alter the value of the locked variable in the loop
+        //the lockState is never unlocked if there is a value that doesn't match the SECRET_COMBO
+        isSame = false;
+      }
+    }
+
+    //we will check if the lock is unlocked
+    if(isSame){
+      lockState.locked = false;
+      redirect('pulkit-arya');
+    }
+  }
 }
 
 // let our other modules find our functions
