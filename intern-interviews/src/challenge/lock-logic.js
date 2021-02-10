a1fcb1bf694dc4cf0a64e8a92273df329ec5aa4c
espinalk212 <espinalk212@gmail.com>
2021-02-10 11:03:30 -0500

    add lock logic, profile page, css and photo

--- intern-interviews/src/challenge/lock-logic.js
+++ intern-interviews/src/challenge/lock-logic.js
@@ -1,13 +1,13 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
-  wheels: [0, 0, 0, 0]
-})
+  wheels: [0, 0, 0, 0],
+});
 
-function changeDialValue (index, incrementBy) {
+function changeDialValue(index, incrementBy) {
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
   // it will be called with a wheel index and an amount to change the value by
@@ -15,12 +15,12 @@ function changeDialValue (index, incrementBy) {
   // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
   // for example, if the user clicked the "down" arrow for the last wheel
   // this will be called with arguments (3, -1).
-  if (lockState.wheels[index] === 0 && incrementBy === -1) return
+  if (lockState.wheels[index] === 0 && incrementBy < 1) return;
 
   if (lockState.wheels[index] >= 0 && lockState.wheels[index] < 9) {
-    lockState.wheels[index] += incrementBy
+    lockState.wheels[index] += incrementBy;
   } else {
-    lockState.wheels[index] = 0
+    lockState.wheels[index] = 0;
   }
 
   // to change the state of the lock, simply make a call like
@@ -32,14 +32,14 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
-  const userValues = [...lockState.wheels]
-  const correctValues = userValues.every(val => SECRET_COMBO.includes(val))
+  const userValues = [...lockState.wheels];
+  const correctValues = userValues.every(val => SECRET_COMBO.includes(val));
   if (correctValues) {
-    lockState.locked = false
-    redirect('kevin-espinal')
+    lockState.locked = false;
+    redirect('kevin-espinal');
   }
 }
 
 // let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
