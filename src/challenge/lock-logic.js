f0c5389ea594df092f44d3201c3e61696408ab70
Mayankbazari <201851065@iiitvadodara.ac.in>
2021-02-10 16:35:50 +0530

    Mayank Bazari Intern Application

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -1,32 +1,28 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
-  wheels: [0, 0, 0, 0]
-})
+  wheels: [0, 0, 0, 0],
+});
 
-function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+function changeDialValue(index, incrementBy) {
+  var temp = "myText" + index;
+  var num =
+    parseInt(document.getElementById(temp).value) + parseInt(incrementBy);
+  document.getElementById(temp).value =
+    parseInt(document.getElementById(temp).value) + parseInt(incrementBy);
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+  lockState.wheels[index] = num;
+  console.log(typeof lockState.wheels, typeof SECRET_COMBO);
+  if (JSON.stringify(lockState.wheels) == JSON.stringify(SECRET_COMBO)) {
+    console.log("yes");
+    lockState.locked = false;
+  }
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+  redirect("Mayank-bazari");
 }
 
-// let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
