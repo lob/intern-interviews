687c5b702de723b9bec41d1ca4e26faad291aa43
YagamiLight <71702983+sagarw749@users.noreply.github.com>
2021-02-16 16:06:25 -0800

    Changed

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -2,31 +2,34 @@ const redirect = window.redirect
 
 const SECRET_COMBO = [1, 3, 5, 1]
 
-const lockState = window.mobx.observable({
-  locked: true,
-  wheels: [0, 0, 0, 0]
-})
+const lockState = window.mobx.observable
+(
+  {
+    locked: true,
+    wheels: [0, 0, 0, 0]
+  }
+)
 
-function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+function changeDialValue (index, incrementBy) 
+{
+  // Function to emulate a numberlock
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
-
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+  lockState.wheels[index] += incrementBy
+  if (compare(0) && compare(1) && compare(2) && compare(3)) 
+  {
+    lockState.locked = false
+    redirect('sagarw')
+  } 
+  else 
+  {
+    lockState.lockState = true
+  }
 }
 
-// let our other modules find our functions
+function compare (index) 
+{
+  return lockState.wheels[index] === SECRET_COMBO[index]
+}
+
 window.lockState = lockState
 window.changeDialValue = changeDialValue
