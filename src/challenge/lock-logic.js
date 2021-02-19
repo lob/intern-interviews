e541c636a35352ba8909d353855a9cbcaf261f60
mehmehmehlol <hkmeganlo@gmail.com>
2021-02-19 15:54:10 -0800

    copied from lob-intern-interviews-2021

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -20,11 +20,20 @@ function changeDialValue (index, incrementBy) {
   // lockState.locked = false
   // or lockState.wheels[1] = 2
   // the lock will re-render itself when the value changes
+  if (incrementBy === 1) {
+    lockState.wheels[index]++
+  } else if (incrementBy === -1) {
+    lockState.wheels[index]--
+  }
 
   // When the lock is set to match the secretCombo
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+  if (lockState.wheels[0] === SECRET_COMBO[0] && lockState.wheels[1] === SECRET_COMBO[1] && lockState.wheels[2] === SECRET_COMBO[2] && lockState.wheels[3] === SECRET_COMBO[3]) {
+    lockState.locked = false
+    redirect('megan-lo')
+  }
 }
 
 // let our other modules find our functions
