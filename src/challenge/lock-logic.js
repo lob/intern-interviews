0f8712e8be6dda57708806edf9ca7c5bce03a25e
mjay768 <MJL!ves4Ever>
2021-02-18 23:21:24 -0600

    Project finished & Material style added

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,25 +8,30 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+  // Value of respective wheel is incremented/decremented
+  lockState.wheels[index] += incrementBy; 
+  console.log("Lock function called");
+  // Checks if the code pattern is matched
+  checkIfCodeMatched();
+  // If unlocked, redirect the page
+  if(lockState.locked == false){
+    redirect("manoj-ponagandla");
+  }
+}
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+const checkIfCodeMatched = () => {
+  if(lockState.wheels[0] == SECRET_COMBO[0] && lockState.wheels[1] == SECRET_COMBO[1] &&
+    lockState.wheels[2] == SECRET_COMBO[2]  && lockState.wheels[3] == SECRET_COMBO[3]){
+      lockState.locked = false;
+    }
+}
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+const redirect_new = () => {
+  console.log("Redirect function called");
+  location.href = "../manoj-ponagandla/index.html";
 }
 
 // let our other modules find our functions
 window.lockState = lockState
 window.changeDialValue = changeDialValue
+window.redirect = "../manoj-ponagandla/index.html"
