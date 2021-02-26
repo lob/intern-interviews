0eb59a6712d5545f13d50e214c348bf2d50e0486
Jonathan Cordero <cryptcur00101@protonmail.com>
2021-02-25 21:51:07 -0500

    Finished solution but had to fork

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -1,32 +1,37 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
   wheels: [0, 0, 0, 0]
-})
+});
 
-function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+// Since there are no loops or iterations,
+// and no arrays and objects created to use memory,
+// Time and Space complexity are constant 0(1)
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+// We can loop through using SECRET_COMBO using array method every()
+// Then Big O will be O(N) where N is the amount of elements in the array
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+function changeDialValue(index, incrementBy) {
+  //When a user clicks on either arrow at any index
+  //the wheel of the index the user is trying to change will increment or decrement by 1
+  lockState.wheels[index] += incrementBy;
+
+  // Once the value of each index matches the value of the same index of SECRET_COMBO
+  // the locked state will be set to false and be redirected
+  if (
+    lockState.wheels[0] === SECRET_COMBO[0] &&
+    lockState.wheels[1] === SECRET_COMBO[1] &&
+    lockState.wheels[2] === SECRET_COMBO[2] &&
+    lockState.wheels[3] === SECRET_COMBO[3]
+  ) {
+    lockState.locked = false;
+    redirect("jonathan-cordero");
+  }
 }
 
 // let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
